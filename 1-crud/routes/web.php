96ba838cdd5fa9adcb7a84b74bede11ebae473f1
Efraim <efrayanglain@gmail.com>
2024-08-26 11:52:01 +0700

    remove DB facade and use model instead

--- 1-crud/routes/web.php
+++ 1-crud/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Models\Guitar;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $guitars = DB::select('select * from guitars');
+    $guitars = Guitar::all();
 
     return view('app', ['guitars' => $guitars]);
 });
