8e0ceb37de3444d8e73a1e8751ff42f3a3094aca
Efraim <efrayanglain@gmail.com>
2024-09-04 14:41:46 +0700

    change layout

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -10,21 +10,37 @@
 </head>
 
 <body>
-    <main class="max-w-4xl mx-auto px-4">
-        <h1>Tags</h1>
-        <p>Kelola semua tag gitar di sini.</p>
+    <main class="max-w-6xl mx-auto px-4 md:grid md:grid-cols-6 gap-4">
+        <header class="col-span-6">
+            <h1>Tags</h1>
+            <p>Kelola semua tag gitar di sini.</p>
 
-        <table class="w-full">
-            <thead>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Aksi</th>
-            </thead>
+            <form hx-post="{{ route('tags.store') }}" hx-swap="afterbegin" hx-target="#list">
+                @csrf
 
-            @foreach ($tags as $tag)
-                <x-tag :tag="$tag" :iteration="$loop->iteration" />
-            @endforeach
-        </table>
+                <label for="name">Nama</label>
+                <input type="text" class="form-control" placeholder="nama" name="name" required>
+
+                <button type="submit" class="py-2 px-4 outline outline-blue-500">Tambah tag baru</button>
+            </form>
+        </header>
+
+        <div class="col-span-6">
+            <table class="w-full">
+                <thead>
+                    <th>Nama</th>
+                    <th>Aksi</th>
+                </thead>
+
+                <tbody id="list">
+                    @foreach ($tags as $tag)
+                        <x-tag :tag="$tag" :iteration="$loop->iteration" />
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+
     </main>
 
 </body>
