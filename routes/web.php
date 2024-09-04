b847722f4713f27840fc0ea22b1a14dd988da5e7
Efraim <efrayanglain@gmail.com>
2024-09-04 14:13:21 +0700

    create new tag controller, model and resource route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GuitarController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/guitars", [GuitarController::class, 'index'])->name("guitars.index");
@@ -8,3 +9,5 @@ Route::post("/guitars", [GuitarController::class, 'store'])->name("guitars.store
 Route::get("/guitars/{id}/edit", [GuitarController::class, 'edit'])->name("guitars.edit");
 Route::put("/guitars/{id}", [GuitarController::class, 'update'])->name("guitars.update");
 Route::delete("/guitars/{id}", [GuitarController::class, 'destroy'])->name("guitars.destroy");
+
+Route::resource("/tags", TagController::class);
