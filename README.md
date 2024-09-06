c75c91f774ed3282b2f76022b66c651aae221041
Efraim <efrayanglain@gmail.com>
2024-09-06 16:28:10 +0700

    setup prettier and blade, tailwind plugins

--- README.md
+++ README.md
@@ -9,15 +9,16 @@ Set up database SQLite dapat dilihat di [dokumentasi resmi
 Laravel](https://laravel.com/docs/11.x/database#sqlite-configuration).
 
 Setelah set up SQLite, perintah berikut akan membuat tabel gitar dan juga
-membuat data gitar awal. 
+membuat data gitar awal.
+
 ```sh
-php artisan migrate --seed 
+php artisan migrate --seed
 ```
 
 Setelah itu, jalankan
 
 ```sh
-php artisan serve 
+php artisan serve
 ```
 
 dan buka `localhost:8000` untuk melihat aplikasinya.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-import daisyui from "daisyui";
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./resources/**/*.{js,css}", "./resources/**/*.blade.php"],
+  content: ['./resources/**/*.{js,css}', './resources/**/*.blade.php'],
   theme: {
     extend: {},
   },
@@ -10,15 +10,15 @@ export default {
     themes: [
       {
         guitardb: {
-          primary: "#38bdf8",
-          secondary: "#0284c7",
-          accent: "#e879f9",
-          neutral: "#111827",
-          "base-100": "#132334",
-          info: "#0067da",
-          success: "#82b300",
-          warning: "#ff9500",
-          error: "#ff6e77",
+          primary: '#38bdf8',
+          secondary: '#0284c7',
+          accent: '#e879f9',
+          neutral: '#111827',
+          'base-100': '#132334',
+          info: '#0067da',
+          success: '#82b300',
+          warning: '#ff9500',
+          error: '#ff6e77',
         },
       },
     ],
